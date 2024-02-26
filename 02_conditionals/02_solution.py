15c80150695d6d39ee3ace6c5d345d1113403a1c
digitalabi <160440877+digitalabi@users.noreply.github.com>
2024-02-26 04:17:30 +0000

    question 2 solution

---
+++ 02_conditionals/02_solution.py
@@ -0,0 +1,13 @@
+# Problem: Movie tickets are priced based on age: $12 for adults (18 and over), $8 for children. Everyone gets a $2 discount on Wednesday.
+
+age = 3
+day = 'Wednesday'
+#day = 'Sunday'
+
+price = 12 if age >= 18 else 8
+
+if day == "Wednesday":
+    price = price - 2
+    #price -=2
+
+print("Ticket price for you is $",price)
