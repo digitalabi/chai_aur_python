12fcd34beb13007be9af4e4ea2a325503945afe7
digitalabi <160440877+digitalabi@users.noreply.github.com>
2024-02-26 04:18:17 +0000

    question 6 solution

---
+++ 02_conditionals/06_solution.py
@@ -0,0 +1,11 @@
+# Problem: Choose a mode of transportation based on the distance (e.g., <3 km: Walkm 3-15 km: Bike, >15 km: Car)
+distance = 15.5
+
+if distance < 3:
+    transport = "Walk"
+elif distance <= 15:
+    transport = "Bike"
+else:
+    transport = "Car"
+
+print(transport)
